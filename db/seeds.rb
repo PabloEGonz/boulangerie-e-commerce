7b046f0e6b3588110e9bdf1946336f53cffaaf91
Pablo <ezekielgonzalezr@gmail.com>
2023-11-29 12:20:01 -0600

    Add seed to database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+Product.create(name: "French Bread", price: 3, category: "Bread", description: "A long loaf of bread with a crispy crust and soft interior.", image: "https://pixnio.com/free-images/2017/09/21/2017-09-21-06-29-57.jpg", stock: 10)
+Product.create!(name: "French Bread", price: 4, category: "Bread", description: "A long loaf of bread with a crispy crust and soft interior.", image: "https://pixnio.com/free-images/2017/09/21/2017-09-21-06-29-57.jpg", stock: 10)
+Product.create!(name: "French Bread", price: 5, category: "Bread", description: "A long loaf of bread with a crispy crust and soft interior.", image: "https://pixnio.com/free-images/2017/09/21/2017-09-21-06-29-57.jpg", stock: 10)
