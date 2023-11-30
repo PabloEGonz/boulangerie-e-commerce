330794c423da29bd1e359435d9c19a76a198f3d2
Pablo <ezekielgonzalezr@gmail.com>
2023-11-29 21:07:04 -0600

    Create basic view for product index

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,6 @@
 #   Character.create(name: "Luke", movie: movies.first)
 
 
-Product.create(name: "French Bread", price: 3, category: "Bread", description: "A long loaf of bread with a crispy crust and soft interior.", image: "https://pixnio.com/free-images/2017/09/21/2017-09-21-06-29-57.jpg", stock: 10)
-Product.create!(name: "French Bread", price: 4, category: "Bread", description: "A long loaf of bread with a crispy crust and soft interior.", image: "https://pixnio.com/free-images/2017/09/21/2017-09-21-06-29-57.jpg", stock: 10)
-Product.create!(name: "French Bread", price: 5, category: "Bread", description: "A long loaf of bread with a crispy crust and soft interior.", image: "https://pixnio.com/free-images/2017/09/21/2017-09-21-06-29-57.jpg", stock: 10)
+Product.create(name: "French Bread", price: 3, category: "Bread", description: "A long loaf of bread with a crispy crust and soft interior.", image: "https://i2.wp.com/epicureandculture.com/wp-content/uploads/2014/12/shutterstock_172040546.jpg", stock: 10)
+Product.create!(name: "French Bread", price: 4, category: "Bread", description: "A long loaf of bread with a crispy crust and soft interior.", image: "https://i2.wp.com/epicureandculture.com/wp-content/uploads/2014/12/shutterstock_172040546.jpg", stock: 10)
+Product.create!(name: "French Bread", price: 5, category: "Bread", description: "A long loaf of bread with a crispy crust and soft interior.", image: "https://i2.wp.com/epicureandculture.com/wp-content/uploads/2014/12/shutterstock_172040546.jpg", stock: 10)
