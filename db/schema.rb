3c024aae4865b1c05351d1cb53cec1f420925f77
Pablo <ezekielgonzalezr@gmail.com>
2023-12-03 12:56:48 -0600

    Create cart item model

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,28 @@
-ActiveRecord::Schema[7.0].define(version: 2023_11_29_171755) do
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema[7.0].define(version: 2023_12_03_174506) do
+  # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cart_items", force: :cascade do |t|
+    t.bigint "shooping_session_id", null: false
+    t.bigint "product_id", null: false
+    t.integer "quantity"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["product_id"], name: "index_cart_items_on_product_id"
+    t.index ["shooping_session_id"], name: "index_cart_items_on_shooping_session_id"
+  end
 
   create_table "orders", force: :cascade do |t|
     t.string "address"
@@ -34,6 +57,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_29_171755) do
     t.integer "stock"
   end
 
+  create_table "shooping_sessions", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.integer "total"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_shooping_sessions_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -46,7 +77,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_29_171755) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "cart_items", "products"
+  add_foreign_key "cart_items", "shooping_sessions"
   add_foreign_key "orders", "products"
   add_foreign_key "orders", "users"
   add_foreign_key "payments", "orders"
+  add_foreign_key "shooping_sessions", "users"
 end
