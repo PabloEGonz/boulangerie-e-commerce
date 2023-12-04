4aa470b8205d4242e03bcf9ad98d31da1c5ca3f0
Pablo <ezekielgonzalezr@gmail.com>
2023-12-04 14:30:56 -0600

    Fix: Make total to default to 0

--- db/migrate/20231203174320_create_shopping_sessions.rb
+++ db/migrate/20231203174320_create_shopping_sessions.rb
@@ -2,7 +2,7 @@ class CreateShoppingSessions < ActiveRecord::Migration[7.0]
   def change
     create_table :shopping_sessions do |t|
       t.references :user, null: false, foreign_key: true
-      t.integer :total
+      t.integer :total, default: 0
 
       t.timestamps
     end
