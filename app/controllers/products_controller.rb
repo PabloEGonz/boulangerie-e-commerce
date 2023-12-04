a96e29b50b3c9e037f6507bc757959e487e4a7c6
Pablo <ezekielgonzalezr@gmail.com>
2023-12-04 14:19:55 -0600

    Feat: Add  actions for products controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,9 +1,12 @@
 class ProductsController < ApplicationController
     def index
     @products = Product.all
+    @shopping_session = current_user.shopping_session || ShoppingSession.create(user_id: current_user.id)
+    @cart_items = @shopping_session.cart_items
     end
 
     def show
     @product = Product.find(params[:id])
+    @cart_item = CartItem.new
     end
 end
