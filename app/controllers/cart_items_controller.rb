f8582bd724a12dfc17042832eabe79012d40568a
Pablo <ezekielgonzalezr@gmail.com>
2023-12-10 16:26:06 -0600

    Refactor: Remove previous method to calculate the total

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -3,7 +3,6 @@ class CartItemsController < ApplicationController
     @cart_item = CartItem.new(cart_item_params)
     @shopping_session = current_user.shopping_session || ShoppingSession.create(user_id: current_user.id)
     @cart_item.shopping_session_id = current_user.shopping_session.id
-    sum_total
     @cart_item.save
     redirect_to "/"
   end
@@ -17,18 +16,12 @@ class CartItemsController < ApplicationController
   def destroy
     @cart_item = CartItem.find(params[:id])
     @cart_item.destroy
-    redirect_to cart_path(current_cart)
+    redirect_to shopping_sessions_path
   end
 
   private
 
   def cart_item_params
     params.require(:cart_item).permit(:quantity, :product_id)
-  end
-
-  def sum_total 
-    price = @cart_item.product.price
-    @shopping_session.total +=  price * params[:cart_item][:quantity].to_i
-    @shopping_session.save
   end
 end
