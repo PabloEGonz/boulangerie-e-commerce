c3d8f9096c4ced0b36d7493374cffc37b1d130ce
Pablo <ezekielgonzalezr@gmail.com>
2023-12-11 16:45:26 -0600

    Refactor: Update cart item lookup in CartItemsController

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -8,9 +8,9 @@ class CartItemsController < ApplicationController
   end
 
   def update
-    @cart_item = CartItem.find(params[:id])
+    @cart_item = CartItem.find_by(product_id: params[:id])
     @cart_item.update(cart_item_params)
-    redirect_to cart_path(current_cart)
+    redirect_to shopping_sessions_path
   end
 
   def destroy
