22e448d9dafb877ee87c8cbb1bdd8548f3d4ecb1
Pablo <ezekielgonzalezr@gmail.com>
2024-02-21 15:10:15 -0600

    Add before_action to initialize shopping session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+    before_action :initialize_shopping_session
+
     def initialize_shopping_session 
         if current_user 
         @shopping_session = current_user.shopping_session || ShoppingSession.create(user_id: current_user.id)
