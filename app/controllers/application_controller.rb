9bd84f58e050a8e5b9c5553c9a0137ae36046a63
Pablo <ezekielgonzalezr@gmail.com>
2024-02-21 15:08:53 -0600

    Add initialize_shopping_session method to ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+    def initialize_shopping_session 
+        if current_user 
+        @shopping_session = current_user.shopping_session || ShoppingSession.create(user_id: current_user.id)
+        end
+    end
 end
