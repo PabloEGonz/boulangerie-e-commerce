d93d6ecb63fec0cbb0d4011a90548652f303db4c
Pablo <ezekielgonzalezr@gmail.com>
2024-03-08 20:05:41 -0600

    Add before_action and set_shopping_session method

--- app/controllers/shopping_sessions_controller.rb
+++ app/controllers/shopping_sessions_controller.rb
@@ -1,14 +1,17 @@
 class ShoppingSessionsController < ApplicationController
+  before_action :set_shopping_session
   def index 
+  end
+  def show  
+  end
+
+  def set_shopping_session
+    @shopping_session = current_user.shopping_session || ShoppingSession.create(user_id: current_user.id)
     if current_user  
-      @shopping_session = current_user.shopping_session || ShoppingSession.create(user_id: current_user.id)
       @cart_items = @shopping_session.cart_items.includes(:product)
     else 
       flash[:notice] = "Please login to continue"
       redirect_to new_user_session_path
     end
-  end
-  def show
-    
   end
 end
