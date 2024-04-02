36a5ca603a2dd4382ab934f60e1cdb5c57294ee4
Pablo <ezekielgonzalezr@gmail.com>
2024-04-02 12:48:57 -0600

    Add cart items to shopping session in ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
     def initialize_shopping_session 
         if current_user 
         @shopping_session = current_user.shopping_session || ShoppingSession.create(user_id: current_user.id)
+        @cart_items = @shopping_session.cart_items.includes(:product)
         else 
             flash[:notice] = "Please login to continue"
             redirect_to new_user_session_path
--- app/controllers/shopping_sessions_controller.rb
+++ app/controllers/shopping_sessions_controller.rb
@@ -1,7 +1,6 @@
 class ShoppingSessionsController < ApplicationController
   before_action :initialize_shopping_session
   def index 
-    @cart_items = @shopping_session.cart_items.includes(:product)
   end
   def show 
      @order = Order.new
