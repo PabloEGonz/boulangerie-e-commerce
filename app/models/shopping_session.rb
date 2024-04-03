d6d583e880abc8fde3cb9ef4a759d96eccd2e8e0
Pablo <ezekielgonzalezr@gmail.com>
2024-04-03 13:47:00 -0600

    Add dependent: :destroy to has_many :cart_items in ShoppingSession model

--- app/models/shopping_session.rb
+++ app/models/shopping_session.rb
@@ -1,6 +1,6 @@
 class ShoppingSession < ApplicationRecord
   belongs_to :user
-  has_many :cart_items
+  has_many :cart_items , dependent: :destroy
   has_many :products, through: :cart_items
 
   def total 
